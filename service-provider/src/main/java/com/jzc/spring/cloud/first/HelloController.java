879bc632e837f0054ccf12068dd09e3ecc6865d3
jiangzhichao <jiangzhichao@yryz.com>
2017-12-15 17:51:36 +0800

    增加ribbon负载均衡

--- service-provider/src/main/java/com/jzc/spring/cloud/first/HelloController.java
+++ service-provider/src/main/java/com/jzc/spring/cloud/first/HelloController.java
@@ -2,6 +2,7 @@ package com.jzc.spring.cloud.first;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ public class HelloController {
 
     private Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    @Value(value = "${server.port}")
+    private String servicePort;
+
     @RequestMapping(value = "hello")
     public String hello() {
-        return "hello, Spring cloud";
+        return "hello, Spring cloud "+"[port]"+servicePort;
     }
 
 }
